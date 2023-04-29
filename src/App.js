d61f9406707a3b9fe7d9856e659714baf7d75ce7
Aleksandr <oscherniavskii@gmail.com>
2023-04-29 18:47:32 +0300

    Проверка взаимодействия с ботом

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { useEffect } from 'react';
 import './App.css';
+//Глобальная переменная (после добавление скрипта в index.html)
+const tg = window.Telegram.WebApp;
+
 
 function App() {
+
+	useEffect(() => {
+		//Метод сообщает, что приложение полностью инициализировалось и его можно отрисовывать
+		tg.ready();
+	}, []);
+
+	const onClose = () => {
+
+		//Закрыть вебприложение
+		tg.close();
+	}
+
 	return (
 		<div className="App">
-			jhdjfhdjhfdf
+			<button onClick={onClose}>Закрыть</button>
 		</div>
 	);
 }
