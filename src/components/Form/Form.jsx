86a5de4a13125beb740bc631e28e7585a82cfc17
Aleksandr <oscherniavskii@gmail.com>
2023-04-30 13:20:10 +0300

    проверка

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -38,26 +38,7 @@ const Form = (props) => {
 
 	return (
 		<div className={'form'}>
-			<h3>Введите ваши данные</h3>
-			<input
-				className={'input'}
-				type="text"
-				placeholder={'Страна'}
-				value={country}
-				onChange={onChangeCountry} />
-			<input
-				className={'input'}
-				type="text"
-				placeholder={'Улица'}
-				value={street}
-				onChange={onChangeStreet} />
-			<select
-				className={'select'}
-				value={subject}
-				onChange={onChangeSubject}>
-				<option value={'physical'}>Физ. лицо</option>
-				<option value={'legal'}>Юр. лицо</option>
-			</select>
+			Введите ваши данные
 		</div>
 	);
 };
