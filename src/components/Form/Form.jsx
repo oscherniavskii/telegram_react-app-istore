10dd042edfb6d01fa0f85e2e5c9fa82ffdcf7159
Aleksandr <oscherniavskii@gmail.com>
2023-04-30 12:24:24 +0300

    Расширение структуры

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './form.css';
+
+
+const Form = (props) => {
+
+	return (
+		<div className='qwqw'>
+
+		</div>
+	);
+};
+
+export default Form;
---
+++ src/components/ProductList/ProductList.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './productList.css';
+
+
+const ProductList = (props) => {
+
+	return (
+		<div className='qqew'>
+
+		</div>
+	);
+};
+
+export default ProductList;
